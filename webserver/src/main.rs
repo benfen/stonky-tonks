b90ac0893ecc32b73732e43fe267c355d5222697
benfenny <benfenny@protonmail.com>
2021-02-28 21:41:51 -0700

    Add optional parsing of the username from the URL
    Super secure approach currently

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,5 +1,37 @@
+use actix_web::dev::Payload;
+use actix_web::HttpRequest;
+use std::future::Future;
+use std::pin::Pin;
+use serde::{ Serialize };
+use actix_web::Error;
+use actix_web::FromRequest;
 use actix_web::{ get, HttpResponse, Result };
+use qstring::QString;
+
 use db::{ establish_connection, StockPrice };
+
+#[derive(Debug, Serialize)]
+struct User {
+    name: Option<String>,
+}
+
+impl FromRequest for User {
+    type Config = ();
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output = Result<User, Error>>>>;
+
+    fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
+        let query_string = req.query_string();
+        let parsed_query_string = QString::from(query_string);
+        let user = User {
+            name: parsed_query_string.get("username").map(|x| x.to_string())
+        };
+
+        Box::pin(async move {
+            Ok(user)
+        })
+    }
+}
 
 #[get("/prices")]
 async fn index() -> Result<HttpResponse> {
@@ -10,6 +42,11 @@ async fn index() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().json(prices))
 }
 
+#[get("/whoami")]
+async fn whoami(user: User) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().json(user))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     use actix_web::{App, HttpServer};
@@ -17,6 +54,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
+            .service(whoami)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
